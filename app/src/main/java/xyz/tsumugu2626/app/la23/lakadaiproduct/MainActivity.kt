6c06524e7cd63eac16a707d9446337bebee2a6f7
Tsumugu Yamaguchi <tsumugu1515@gmail.com>
2023-02-20 14:13:05 +0900

    first commit

---
+++ app/src/main/java/xyz/tsumugu2626/app/la23/lakadaiproduct/MainActivity.kt
@@ -0,0 +1,14 @@
+package xyz.tsumugu2626.app.la23.lakadaiproduct
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
